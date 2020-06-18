5dced42c2db2da1988a7254f7c3c1c69af04c3ea
matt-plank <matthew_plank@outlook.com>
2020-06-18 19:21:30 +0100

    Removed errors and type errors from code

--- src/PyANN/test_utils.py
+++ src/PyANN/test_utils.py
@@ -7,17 +7,17 @@ from PyANN.utils import add_col, remove_col
 
 class TestUtils(unittest.TestCase):
     def test_add_col(self):
-        matrix: np.array = np.ones((5, 5))
+        matrix: np.ndarray = np.ones((5, 5))
 
-        matrix_with_col: np.array = add_col(matrix)
+        matrix_with_col: np.ndarray = add_col(matrix)
 
         self.assertTrue(matrix.shape[0] == matrix_with_col.shape[0])
         self.assertTrue(matrix.shape[1] == matrix_with_col.shape[1] - 1)
 
     def test_remove_col(self):
-        matrix: np.array = np.ones((5, 5))
+        matrix: np.ndarray = np.ones((5, 5))
 
-        matrix_without_col: np.array = remove_col(matrix)
+        matrix_without_col: np.ndarray = remove_col(matrix)
 
         self.assertTrue(matrix.shape[0] == matrix_without_col.shape[0])
         self.assertTrue(matrix.shape[1] - 1 == matrix_without_col.shape[1])
