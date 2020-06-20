8a522c42ca4e1d9e5e19105f7f9ac0887df185b1
matt-plank <matthew_plank@outlook.com>
2020-06-20 17:24:54 +0100

    Related to #5. Added momentum to testing

--- src/PyANN/test_layers.py
+++ src/PyANN/test_layers.py
@@ -12,6 +12,14 @@ class TestLayers(unittest.TestCase):
         ys: np.ndarray = layer.predict(xs)
 
         self.assertTrue(ys.shape == (xs.shape[0], 22))  # Number of rows is preserved (each row is a datapoint)
+        self.assertTrue(len(ys.shape) == len(xs.shape))  # Output has the same number of demensions as
+
+    def test_Dense_predict_momentum(self):
+        layer: Dense = Dense(3, 22, momentum_rate=0.5)
+        xs: np.ndarray = np.random.random((20, 3))
+        ys: np.ndarray = layer.predict(xs)
+
+        self.assertTrue(ys.shape == (xs.shape[0], 22))  # Number of rows is preserved (each row is a datapoint)
         self.assertTrue(len(ys.shape) == len(xs.shape))  # Output has the same number of demensions as input
 
     def test_Dense_error(self):
@@ -42,7 +50,25 @@ class TestLayers(unittest.TestCase):
         layer: Dense = Dense(2, 1)
         xs: np.ndarray = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
         ys: np.ndarray = layer.predict(xs)
-        desired_ys: np.ndarray = np.array([[0], [1], [1], [0]])
+        desired_ys: np.ndarray = np.array([[0], [1], [1], [1]])
+        network_error: np.ndarray = desired_ys - ys
+
+        layer.apply_delta(xs, network_error, 0.001)
+
+        ys_recaltulated: np.ndarray = layer.predict(xs)
+        error_recalcualted: np.ndarray = desired_ys - ys_recaltulated
+
+        # Applying a delta should reduce error
+        # This requires a seed because sometimes the step is too large
+        self.assertTrue(error_recalcualted.sum() <= network_error.sum())  # Applying a delta should always reduce error
+
+    def test_Dense_apply_delta_momentum(self):
+        np.random.seed(100)
+
+        layer: Dense = Dense(2, 1, momentum_rate=0.5)
+        xs: np.ndarray = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+        ys: np.ndarray = layer.predict(xs)
+        desired_ys: np.ndarray = np.array([[0], [1], [1], [1]])
         network_error: np.ndarray = desired_ys - ys
 
         layer.apply_delta(xs, network_error, 0.001)
